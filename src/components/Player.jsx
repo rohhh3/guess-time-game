e1c59c37dd8455f252c9be97f181b37461bd6571
Krzysztof Pawlowski <krzysztofpeiv@o2.pl>
2024-04-13 19:51:14 +0200

    added user input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,20 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+  }
+
   return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
+    <section id='player'>
+      <h2>Welcome {enteredPlayerName ?? "Guest"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type='text' />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
